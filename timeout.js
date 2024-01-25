dd4f007a391a97d50e4e70c0e0b68ed8b879d769
developertamimtaz <farhanahmedtaz21@gmail.com>
2024-01-25 22:10:15 +0600

    js timeout

--- timeout.js
+++ timeout.js
@@ -1,6 +1,10 @@
+//JavaScript Timeout.
 function doSomething(){
     console.log(3333);
 }
 console.log(2222);
-setTimeout(doSomething)
+setTimeout(doSomething, 10000)
+console.log(4444);
+console.log(4444);
+console.log(4444);
 console.log(4444);
